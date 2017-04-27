400a0082dea7799aad9f81bb0199ad0b553209ce
mickityy <mickityy@gmail.com>
2017-04-27 23:52:12 +0300

    Portfolio images and scroll animation

--- js/mylp.js
+++ js/mylp.js
@@ -3,3 +3,10 @@
        $('.mymenu').slideToggle();
     });
   });
+
+  $(document).ready(function() {
+      $('.scrolldown').click(function(event){
+          $('html, body').animate({scrollTop: '+=700px'}, 800);
+      });
+      return false;
+  });
